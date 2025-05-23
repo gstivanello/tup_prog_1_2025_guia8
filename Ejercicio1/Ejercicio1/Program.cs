c02b6b2d7a40b8b4e8ed79e95b8283cb7e03fd4d
gstivanello <stivanellogonzalo@gmail.com>
2025-05-23 10:47:52 -0300

    Prueba en clase

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Declaracion de variables
+            int contador, acumulador, cantidad;
+            int num, maximo, minimo;
+            double prom;
+            #endregion
+
+            #region Inicializacion de variables
+            contador = 0;
+            acumulador = 0; 
+            maximo = 0;
+            minimo = 10000;
+
+            #endregion
+
+            #region Solicitar cantidad de ingresos
+            Console.WriteLine("Ingrese la cantidad de ingresos");
+            cantidad = Convert.ToInt32(Console.ReadLine());
+            #endregion
+
+            #region Iterar ingresos
+            if (cantidad != 0)
+            {
+                for (int i = 0; i < cantidad; i++)
+                {
+                    Console.WriteLine("Ingrese un valor");
+                    num = Convert.ToInt32(Console.ReadLine());
+                    acumulador = acumulador + num;
+                    contador = contador++;
+
+                    if (contador == 1 || maximo > num)
+                    {
+                        maximo = num;
+                    }
+                    else
+                    if (contador == 1 || minimo < num)
+                    {
+                        minimo = num;
+                    }
+                }
+                #region Mostrar en pantalla 
+                prom = acumulador / cantidad;
+                Console.WriteLine($"El maximo es: {maximo} \n El minimo es: {minimo} \n El promedio es: {prom}");
+                Console.ReadKey();
+                #endregion
+            }
+            #endregion
+
+            else
+            {
+                if (cantidad == 0)
+                {
+                    Console.WriteLine("No se ingresaron numeros");
+                    Console.ReadKey();
+                }
+            }
+
+        }
+    }
+}
---
+++ Ejercicio1/Ejercicio1_guia8/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Ejercicio1_guia8
+{
+    internal class Program
+    {
+        static int acumulador;
+        static int contador;
+        static int minimo;
+        static int maximo; 
+
+
+        static void RegistrarValor(int valor)
+        {
+            acumulador += valor;
+            contador++;
+        }
+
+        static double CalculadorPromedio()
+        {
+            double promedio = 0;
+            if (contador > 0)
+            {
+                promedio = 1d * acumulador / contador;
+            }
+            return promedio;
+        }
+
+        static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Ejercicio1 - Ingrese 1,2,3 segun menú");
+            Console.WriteLine("1- Procesar un solo número");
+            Console.WriteLine("2- Procesar varios números");
+            Console.WriteLine("3- Mostrar máximo y mínimo");
+            Console.WriteLine("4- Mostrar promedio");
+
+            int op = Convert.ToInt32(Console.ReadLine());
+
+            return op;
+
+        }
+
+        static void MostrarPantallaSolicitudNumero()
+        {
+
+            Console.Clear();
+            Console.WriteLine("Ejercicio1 - solicitud de un solo número");
+
+            int numero = Convert.ToInt32(Console.ReadLine());
+            RegistrarValor(numero);
+
+        }
+
+        static void MostrarPantallaCalcularYMostrarPromedio()
+        {
+
+            Console.Clear();
+            Console.WriteLine("Ejercicio1 - Calcular promedio");
+
+            double promedio = CalculadorPromedio();
+
+            Console.WriteLine($"Promedio {promedio}");
+
+            Console.WriteLine("Presione cualquier tecla para volver al menú principal");
+            Console.ReadKey();
+
+        }
+        static void Main(string[] args)
+        {        
+            int op = MostrarPantallaSolicitarOpcionMenu(); 
+
+            while (op != -1)
+            {
+                switch (op)
+                {
+                    case 1: Console.WriteLine("");
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+
+                }
+            }
+            
+        }
+    }
+}
